3502a3353b3b7fa2cccc78253a1951f8452b564a
Halie Murray-Davis <hmurraydavis@yahoo.com>
2015-09-20 18:41:27 -0400

    Remove Pandas and use numpy. Write to a pickle file!

--- dataSetup.py
+++ dataSetup.py
@@ -5,10 +5,23 @@ Created on Thu Sep 17 15:13:27 2015
 @author: hdavis
 """
 
-import pandas as pd
+##import pandas as pd
 import numpy as np
+import pickle
 #download data from: http://databank.worldbank.org/data/download/WDI_csv.zip
 
-data=pd.read_csv('WDI_Data.csv')
+wdi_all_data = np.genfromtxt('WDI_Data.csv', delimiter=',',usecols=np.arange(0,60))
 
-print data.groupby('Indicator Name')
+pickle.dump(wdi_all_data, open( "wdi_all_data.p", "wb" ) )
+print "Full WDI Data has been read in and saved to file!"
+
+
+
+###Depricated Pandas stuff:
+
+#data=pd.read_csv('WDI_Data.csv')#, 'Improved sanitation facilities (% of population with access)')
+#data
+
+#d2 = data.groupby('Indicator Name')
+#print dir(d2)
+#d2['Improved sanitation facilities (% of population with access)']
