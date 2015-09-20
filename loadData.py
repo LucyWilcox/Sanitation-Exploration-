59092847e4059fbc5de45944a77755fa7d546360
Halie Murray-Davis <hmurraydavis@yahoo.com>
2015-09-20 18:48:40 -0400

    Open pickeled data for data analytics fun!

---
+++ loadData.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 20 18:44:57 2015
+
+@author: hdavis
+"""
+
+import numpy as np
+import pickle
+
+wdi_data = pickle.load( open( "wdi_all_data.p", "rb" ) )
+
+print type(wdi_data[1])
