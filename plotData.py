6aff67d68551b4b6f69cc209e78ce55787be2877
unknown <hdavis@CTVPFV1.olin.edu>
2015-09-23 23:53:00 -0400

    Plot two variables against each other with best fit line

--- plotData.py
+++ plotData.py
@@ -2,6 +2,7 @@ import RAD_Sanitation as RADS
 import pickle
 import matplotlib.pyplot as plt
 import pprint
+import numpy as np
 
 #[(Country, indicator, [values],[years]), (Country, indicator, [values],[years]),...]
 
@@ -21,7 +22,12 @@ prog2ndSclFmlList = RADS.ourCountries(progSecndrySclFemale,country_list)
 
 d1,d2,years = RADS.yearCorrelate(prog2ndSclFmlList[1], impSanitation[1])
 
-plt.plot(d1,d2, '*', linewidth=8)
+plt.plot(d1,d2, '*', markersize=20)
+
+d1 = np.array(d1); d2=np.array(d2)
+m,b = np.polyfit(d1, d2, 1)#, full=True)
+
+plt.plot(d1,m*d1+b, linewidth=8)
 
 # plt.plot(prog2ndSclFmlList[1][3],prog2ndSclFmlList[1][2], linewidth=8)
 plt.show()
