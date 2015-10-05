92ac37b2361bf30eb20a73827ddb46a664a46d24
Halie Murray-Davis <hmurraydavis@yahoo.com>
2015-10-05 14:17:14 -0400

    Variable axis scales and code refactoring

--- plotData.py
+++ plotData.py
@@ -16,19 +16,15 @@ data_values = {'% Gross Female Secondary School Enrollment': "dataPickles/grossS
 	'% Access to Contraceptives': "dataPickles/contraceptivePrevalance.p"
 }
 
-little_plots = True
- #Plot the little plots of the scatter data with different time offsets
+
+little_plots = True # :Plot the little plots of the scatter data with different time offsets
+axies_scale = 'linear' # :Plot nad compute the values with log/log axies. Options include: 'log_log'
+
 variable_name_plot_dependent = 'Improved Water Source (% access)'#'Improved Sanitation Facilities (% access)'#
 variable_name_plot_independent = '% Gross Female Secondary School Enrollment'
 
 #[(Country, indicator, [values],[years]), (Country, indicator, [values],[years]),...]
 
-# ##Load data from Pickle files of segmented data:
-# impSanitation = pickle.load( open( "dataPickles/ImprovedSan.p", "rb" ) )
-# contraceptPrev = pickle.load( open( "dataPickles/contraceptivePrevalance.p", "rb" ) )
-# impWaterSrc = pickle.load( open( "dataPickles/improvedWaterSource.p", "rb" ) )
-# # progSecndrySclFemale = pickle.load( open( "dataPickles/progSecondarySchoolFemale.p", "rb" ) )
-# grossSecndrySclFemale = pickle.load( open( "dataPickles/grossSecondarySchoolEnrolment.p", "rb" ) )
 
 dependent = pickle.load( open( data_values[variable_name_plot_dependent], "rb" ) )
 independent = pickle.load( open( data_values[variable_name_plot_independent], "rb" ) )
@@ -40,17 +36,17 @@ dependent = RADS.ourCountries(dependent, country_list)
 independent = RADS.ourCountries(independent, country_list)
 
 
+
 def crossCorrelateValuesForPlotting(data1, data2, countryIndex, yearOffset):
 	#pprint.pprint(data2[countryIndex])
 	d1,d2,years = RADS.yearCorrelate(data1[countryIndex], data2[countryIndex], yearOffset = yearOffset)
 
 	d1 = np.array(d1); d2=np.array(d2)
 	if (len(d1)>1) and (len(d2)>1):
-		m,b = np.polyfit(d1, d2, 1)
-		return d1, d2, m, b
+		return d1, d2
 	else:
 		#print 'Country ', countryIndex, ' has no data for given indicator.'
-		return d1, d2, False, False
+		return d1, d2
 
 
 rsquaredCumulative = []; pvalCumulative = []; stderrorCumulative = []; yearOffsetCumulative = []; mCumulative = []; pearsonCorrelationCumulative = []
@@ -58,42 +54,45 @@ for yearOffset in range(-1,1):
 	print '\n Year offset is: ', yearOffset
 	d1_all = []; d2_all = []
 	for country_index, country_name in enumerate(country_list):
-		d1, d2, m, b = crossCorrelateValuesForPlotting(dependent, independent, country_index, yearOffset)
+		d1, d2 = crossCorrelateValuesForPlotting(dependent, independent, country_index, yearOffset)
 		if  (len(d1)>0):
-			# line, = ax.plot(d1, d2, marker = 'o', color='blue', lw=2) #plt.plot(d1,d2, '*', markersize=20, label = country_name)
 			d1_all.append(d1); d2_all.append(d2)
-		# if (m != False):
-			# plt.plot(d1,m*d1+b, linewidth=8)
+
 
 	d1_log = []
+	d2_log = []	
+
+	d1_values = []
+	d2_vlaues = []
+
 	for array in d1_all:
 		for nested_value in array:
 			d1_log.append(np.log10(nested_value))
-	d2_log = []	
+			d1_values.append(nested_value)
+
 	for array in d2_all:
 		for nested_value in array:
 			d2_log.append(np.log10(nested_value))
+			d2_vlaues.append(nested_value)
+
+	# pprint.pprint(d2_log)
 	d1_log = np.array(d1_log)
 	d2_log = np.array(d2_log)
+	d1_values = np.array(d1_values)
+	d2_vlaues = np.array(d2_vlaues)
+
+	print d1_log
 
 
 	# line, = ax.plot(d1, m*d1+b, marker = 'o', color='red', lw=8)#plt.plot(d1,m*d1+b, linewidth=8)
-	m, b, r_value, p_value, std_err = scipy.stats.linregress(d1_log, d2_log)
-	if little_plots==True:
-		plt.plot(d1_log,d2_log, 'o', markersize=13, alpha=.5)
-		plt.plot(d1_log, m*d1_log+b, linewidth=5)
-		# plt.show()
-		plt.ylabel(variable_name_plot_independent+' (log base 10)', fontsize=18)
-		plt.xlabel(variable_name_plot_dependent+' (log base 10)', fontsize=18)
-		plt.suptitle('Effect of '+variable_name_plot_dependent+' \n on '+variable_name_plot_independent, fontsize=20)
-		plt.title('$R^2$ value of: '+str(r_value**2) + ', $P$ value of: '+str(p_value)+', Year Offset = '+str(yearOffset), fontsize=13) ##TODO: Fix text so it shows up all the time!!!
-		# plt.legend()
-		# plt.plot(independent[1][3],independent[1][2], linewidth=8)
-
-		plt.show()
-		#plt.savefig(variable_name_plot_dependent+'_offsetScatterPlot_offsetOf'+str(yearOffset)+'.png', papertype='letter')
-		plt.clf()
-	pearsonCorrelation = scipy.stats.pearsonr(d1_log, d2_log)
+	if axies_scale=='log_log':
+		x = d1_log
+		y = d2_log
+	if axies_scale=='linear':
+		x=d1_values
+		y=d2_vlaues
+	m, b, r_value, p_value, std_err = scipy.stats.linregress(x, y)
+	pearsonCorrelation = scipy.stats.pearsonr(x, y)	
 	print 'r squared: ', r_value**2
 	print 'p val: ', p_value
 	print 'standard error: ', std_err
@@ -107,6 +106,25 @@ for yearOffset in range(-1,1):
 	yearOffsetCumulative.append(yearOffset);
 	mCumulative.append(50*m)
 	pearsonCorrelationCumulative.append(pearsonCorrelation)
+
+	if little_plots==True:
+		plt.clf()
+		plt.plot(x, y, 'o', markersize=13, alpha=.5)
+		plt.plot(x, m*x+b, linewidth=5)
+		# plt.show()
+		plt.ylabel(variable_name_plot_independent+' (log base 10)', fontsize=18)
+		plt.xlabel(variable_name_plot_dependent+' (log base 10)', fontsize=18)
+		plt.suptitle('Effect of '+variable_name_plot_dependent+' \n on '+variable_name_plot_independent, fontsize=20)
+		subtitle = '$R^2$ value of: '+str(r_value**2) + ', $P$ value of: '+str(p_value)+', Year Offset = '+str(yearOffset)
+		plt.title(subtitle, fontsize=13) ##TODO: Fix text so it shows up all the time!!!
+		# plt.legend()
+		# plt.plot(independent[1][3],independent[1][2], linewidth=8)
+
+		plt.show()
+		#plt.savefig(variable_name_plot_dependent+'_offsetScatterPlot_offsetOf'+str(yearOffset)+'.png', papertype='letter')
+		
+	
+
 
 
 
