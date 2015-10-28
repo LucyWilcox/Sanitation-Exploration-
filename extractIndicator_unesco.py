b0e5e6b5a054f67780eb14fa54b6562c313c2ec3
Halie Murray-Davis <hmurraydavis@yahoo.com>
2015-10-28 02:45:29 -0400

    Tested and pickles data. Works.

--- extractIndicator_unesco.py
+++ extractIndicator_unesco.py
@@ -4,8 +4,10 @@ import math
 import matplotlib.pyplot as plt
 import pprint
 
-desiredIndicator = 'Percentage of primary schools with no information on electricity (%)'
-desiredCode = b'SCHBSP_2_PU_MIXTOIL'
+desiredIndicator = 'Percentage of lower secondary schools with toilets (%)'
+desiredCode = b'SCHBSP_2_PU_WTOIL'
+
+set_nan_to_zero = True
 
 ##Load pickled data files. If these aren't present, run dataSetup.py
 all_data_numbers = np.load('BulkData/unesco_all_data_numbers.npy')
@@ -22,26 +24,43 @@ valueIndex = 6
 counryList = []
 
 for rowNumberText, rowText in enumerate(all_data_text):
+    
+    ## Grab out/unpack fields:
     rowNumberNumbers = rowNumberText+1
     country = rowText[3]
     year = rowText[4]
     indicator = str( rowText[1] )
     code = str( rowText[0] )
     value = all_data_numbers[rowNumberNumbers][valueIndex]
-    code = code.strip('\"')
     
-    print 'country is: ', country
+    ## Remove quotation marks from front and back of string
+    code = code.strip('\"')
+    country = country.strip('\"')
+    
 
-    if code == desiredCode:
-        print 'Yeah!!!'
-        #print country, indicator, year, value
-        
+    
+    ## Only write the data for the indicator we want: 
+    if code == desiredCode: 
+    
+        ## If specified, set NaN values to zero:
+        if ( (set_nan_to_zero==True) and math.isnan(value) ):
+            value=0
+     
+        ## Add the country to the data structure if it isn't there
         if not country in counryList:
             data.append( (country, indicator, [value], [year]) )
             counryList.append(country)
+        ## Update country data lists if they already present
         else:
-            print data[ counryList.index(country) ][2].append(value)
+            data[ counryList.index(country) ][2].append(value)
             data[ counryList.index(country) ][3].append(year)
     
     
-        pprint.pprint( data )
+pprint.pprint( data )
+
+pickle.dump(data, open( "dataPickles/unesco_"+desiredIndicator+".p" , "wb" ) )
+
+##for entry in data:
+###    print entry
+##    if entry[0]=='Holy See':
+##        print entry
